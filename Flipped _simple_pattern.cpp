d1e705aa24369ff08e7c22437dd630808877430e
codevoyager-12 <abdullahkiller890@gmail.com>
2026-01-04 21:41:59 +0500

    Flipped simple pattern

---
+++ Flipped _simple_pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Enter a number ";
+	int num;
+	cin >> num;
+	cout << endl;
+	for (int i = 0 ;i < num;i++) {
+		for (int j = num;j >= 0;j--) {
+			if (j <= i) {
+				cout << "*";
+			}
+			else { cout << " "; }
+		}
+		cout << endl;
+	}
+}
