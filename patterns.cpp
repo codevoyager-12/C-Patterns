412ef3858cce413dbcf1844d70a82f342651e24e
codevoyager-12 <abdullahkiller890@gmail.com>
2026-01-04 21:19:41 +0500

    Simple pyramid code

--- patterns.cpp
+++ patterns.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-  
+    // Simple pyramid
+  cout<<"Enter the number "<<endl;
+  int num;
+  cin>> num;
+  for(int i=0;i<num;i++){
+    for(int j=0;j<=i;j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+  }
 }
