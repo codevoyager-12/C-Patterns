bd16462282b1f78fd01df46971a38f3d8a881818
codevoyager-12 <abdullahkiller890@gmail.com>
2026-01-05 18:28:21 +0500

    Flipped

---
+++ Flipped_inverted_pyramid.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Enter a number ";
+	int num;
+	cin >> num;
+	cout << endl;
+	for (int i = 0;i < num;i++) {
+		for (int j = 0;j < num;j++) {
+			if (j < i) { cout << "  ";  }//two spaces 
+			else { cout << "* "; }
+		}
+		cout << endl;
+	}
+}
