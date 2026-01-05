0e4711569c90436434f60199ed93098fd136a6d0
codevoyager-12 <abdullahkiller890@gmail.com>
2026-01-05 22:02:55 +0500

    changes done

--- Inverted_pyramid.cpp
+++ Inverted_pyramid.cpp
@@ -1,14 +1,18 @@
+// * * * * * *
+//  * * * * *
+//   * * * *
+//    * * *
+//     * *
+//      *
 #include<iostream>
 using namespace std;
 int main() {
-	cout << "Enter a number ";
+	cout << "Enter the number  " << endl;
 	int num;
 	cin >> num;
-	cout << endl;
 	for (int i = num;i > 0;i--) {
-		for (int j = 0;j < i;j++) {
-			cout << "* ";
-		}
+		for (int k = 0;k < num - i;k++) cout << " ";
+		for (int j = 0;j < i;j++) cout << "* ";
 		cout << endl;
 	}
 }
